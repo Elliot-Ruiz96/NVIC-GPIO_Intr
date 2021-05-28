28b3a6df0ff7027121ddd215b2d50bcb0634809e
Elliot Ruiz Sánchez <elliotruizs@ieee.org>
2021-05-28 09:46:22 -0500

    Update GPIO_Intr.c

--- GPIO_Intr.c
+++ GPIO_Intr.c
@@ -12,7 +12,6 @@ uint8_t g_ButtonPress = 0;
     };
 
 
-
 gpio_pin_config_t led_config = {
         kGPIO_DigitalOutput,
         0,
@@ -31,6 +30,15 @@ void PORTA_IRQHandler(void)
     SDK_ISR_EXIT_BARRIER;
 }
 
+void PORTC_IRQHandler(void)
+{
+
+    GPIO_PortClearInterruptFlags(GPIOC, 1U << PIN6);
+
+    g_ButtonPress = true;
+    SDK_ISR_EXIT_BARRIER;
+}
+
 
 int main(void) {
 
@@ -44,16 +52,28 @@ int main(void) {
 	    kPORT_UnlockRegister                                     /* Pin Control Register fields [15:0] are not locked */
 	  };
 
+	const port_pin_config_t portc_pin6_config = {
+		    kPORT_PullUp,                                            /* Internal pull-up resistor is enabled */
+		    kPORT_FastSlewRate,                                      /* Fast slew rate is configured */
+		    kPORT_PassiveFilterDisable,                              /* Passive filter is disabled */
+		    kPORT_OpenDrainDisable,                                  /* Open drain is disabled */
+		    kPORT_HighDriveStrength,                                 /* High drive strength is configured */
+		    kPORT_MuxAsGpio,                                         /* Pin is configured as PTA4 */
+		    kPORT_UnlockRegister                                     /* Pin Control Register fields [15:0] are not locked */
+		  };
 
   CLOCK_EnableClock(kCLOCK_PortA);
   CLOCK_EnableClock(kCLOCK_PortB);
   CLOCK_EnableClock(kCLOCK_PortC);
 
   PORT_SetPinConfig(PORTA, PIN4, &porta_pin4_config);
+  PORT_SetPinConfig(PORTC, PIN6, &portc_pin6_config);
 
   PORT_SetPinInterruptConfig(PORTA, PIN4, kPORT_InterruptFallingEdge);
+  PORT_SetPinInterruptConfig(PORTC, PIN6, kPORT_InterruptFallingEdge);
 
   GPIO_PinInit(GPIOA, PIN4, &sw_config);
+  GPIO_PinInit(GPIOC, PIN6, &sw_config);
 
 
   PORT_SetPinMux(PORTB, PIN22, kPORT_MuxAsGpio);
@@ -65,8 +85,12 @@ int main(void) {
 
 
   NVIC_EnableIRQ(PORTA_IRQn);
+  NVIC_EnableIRQ(PORTC_IRQn);
+
   	/**It Sets the priority of the IRQ*/
+
   NVIC_SetPriority(PORTA_IRQn, 2);
+  NVIC_SetPriority(PORTC_IRQn, 4);
 
     while(1) {
 
